ee3d90f9ec9f9ac9225b0afe9b3ed28c373473db
Brijendra Singh <brijj2006@gmail.com>
2026-05-28 01:06:10 +0800

    added maps code

--- src/main/java/org/example/Maps.java
+++ src/main/java/org/example/Maps.java
@@ -1,4 +1,35 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Maps {
+
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "apple");
+        map.put(2, "banana");
+        map.put(3, "cherry");
+        map.put(4, "tomato");
+        map.put(5, "avocado");
+
+        System.out.println(map);
+
+        Set<Integer> keys = map.keySet();
+        for (Integer key : keys) {
+            System.out.println(map.get(key));
+        }
+
+        Set<Map.Entry<Integer, String>> entries = map.entrySet();
+        for (Map.Entry<Integer, String> entry : entries) {
+            System.out.println("Key : " + entry.getKey() + " | Value : " + entry.getValue());
+            entry.setValue(entry.getValue().toUpperCase());
+        }
+
+        System.out.println(map);
+
+    }
+
+
 }
